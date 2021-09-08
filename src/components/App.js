293afe4b4fd248ca55499a43df768ae09544a7fc
Kasia Gierat <kasia.gierat@gmail.com>
2021-09-08 16:22:28 -0400

    filter countries while writing

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,52 @@ const regions = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
+
+  // errors, loading
   const [countryNotFound, setCountryNotFound] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+
+  const [filterByRegionTerm, setFilterByRegionTerm] = useState("");
+  const [filterByCountryNameTerm, setFilterByCountryNameTerm] = useState("");
 
   useEffect(() => {
     getCountries("all");
   }, []);
+
+  // handling error when country is not found
+  useEffect(() => {
+    // if 0 countries and isLoading are displayed, show the error "No country found"
+    if (filteredCountries.length === 0 && !isLoading) {
+      setCountryNotFound(true);
+    }
+    // if countries are displayed and error is true, flip the error to false
+    else if (filteredCountries.length && countryNotFound) {
+      setCountryNotFound(false);
+    }
+  }, [filteredCountries]);
+
+  // filtering based on the keyword in SearchBar and chosen option from Dropdowan
+  useEffect(() => {
+    let countryName;
+    let searchCountryName;
+
+    const filtered = countries.filter((country) => {
+      countryName = country.name.toLowerCase();
+      searchCountryName = filterByCountryNameTerm.toLowerCase();
+      if (filterByRegionTerm && filterByCountryNameTerm) {
+        return (
+          countryName.includes(searchCountryName) &&
+          country.region === filterByRegionTerm
+        );
+      } else if (filterByRegionTerm) {
+        return country.region === filterByRegionTerm;
+      } else {
+        return countryName.includes(searchCountryName);
+      }
+    });
+    setFilteredCountries(filtered);
+  }, [filterByRegionTerm, filterByCountryNameTerm]);
+ 
 
   const getCountries = async (searchTerm) => {
     try {
@@ -27,29 +67,16 @@ const App = () => {
     }
     setIsLoading(false);
   };
-
-  const filterCountries = (region) => {
-    const filtered = countries.filter((country) => {
-      return country.region === region;
-    });
-    setFilteredCountries(filtered);
-  };
-
-  const getCountry = async (searchTerm) => {
-    try {
-      const response = await countriesApi.get(`/name/${searchTerm}`);
-      setFilteredCountries(response.data);
-      setCountryNotFound(false);
-    } catch (error) {
-      setCountryNotFound(true);
-    }
-  };
+  
 
   return (
     <Container>
       <Grid stackable style={{ marginBottom: "1rem" }}>
         <Grid.Column floated="left" width={5}>
-          <SearchBar onSubmit={getCountry} title={"Search country"} />
+          <SearchBar
+            onChange={setFilterByCountryNameTerm}
+            title={"Search country"}
+          />
           {countryNotFound ? (
             <Header as="h5" style={{ marginTop: "5px", color: "red" }}>
               Country not found
@@ -60,7 +87,7 @@ const App = () => {
         <Grid.Column floated="right" width={5}>
           <Dropdown
             options={regions}
-            onChange={filterCountries}
+            onChange={setFilterByRegionTerm}
             label={"Filter by region"}
           />
         </Grid.Column>
@@ -71,7 +98,6 @@ const App = () => {
       ) : (
         <CountriesList countries={filteredCountries} />
       )}
-      {/* <CountriesList countries={filteredCountries} /> */}
     </Container>
   );
 };
