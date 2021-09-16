12ef50e516891594bc7d9e95ff625480f94bb7ba
Kasia Gierat <kasia.gierat@gmail.com>
2021-09-16 18:06:24 -0400

    debounce lodash

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,6 @@ import { Form } from "semantic-ui-react";
 
 const SearchBar = (props) => {
   const [keyword, setKeyword] = useState("");
-  // console.log("keyword", keyword);
-  // const onFormSubmit = (e) => {
-  //   e.preventDefault();
-  //   props.onSubmit(keyword);
-  //   setKeyword("");
-  // };
 
   return (
     <Form>
@@ -19,7 +13,6 @@ const SearchBar = (props) => {
           type="text"
           value={keyword}
           onChange={(e) => {
-            // console.log(e.target.value);
             setKeyword(e.target.value);
             props.onChange(e.target.value);
           }}
